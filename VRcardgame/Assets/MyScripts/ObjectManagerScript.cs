b5306f1ad7c1f105599de70cfb5e1d818de9651b
mirori996 <33239449+mirori996@users.noreply.github.com>
2021-01-16 01:29:40 +0900

    add photon script

---
+++ VRcardgame/Assets/MyScripts/ObjectManagerScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ObjectManagerScript : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public Vector3 myCubePos;
+    
+
+    private GameManagerScript gameManagerScript;
+
+    private void Awake()
+    {
+        gameManagerScript = GameObject.FindWithTag("GameManager").GetComponent<GameManagerScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            if (gameManagerScript.isVR == false)
+            {
+                myCubePos = gameManagerScript.cube.transform.position;
+                stream.SendNext(myCubePos);
+                print("AR上のmyCubePosを送信しました：" + myCubePos);
+
+            } else
+            {
+                
+            }
+        }
+        else
+        {
+            if (gameManagerScript.isVR == false)
+            {
+
+            }
+            else
+            {
+                myCubePos = (Vector3)stream.ReceiveNext();
+                print("VRアプリがAR上のmyCubePosを受信しました：" + myCubePos);
+                gameManagerScript.cube.transform.position = myCubePos;
+                print("VRアプリがAR上のmyCubePosをgameManagerのcubeのpositionに格納しました：" + gameManagerScript.cube.transform.position);
+            }
+
+        }
+    }
+}
