0e9965b2b6ae7a42461359907fc88a4232c82e97
mirori996 <33239449+mirori996@users.noreply.github.com>
2021-01-16 12:36:41 +0900

    fix script

---
+++ VRcardgame/Assets/GameManagerScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManagerScript : MonoBehaviourPunCallbacks
+{
+    public GameObject cube;
+    public bool isVR = true;
+    //public int player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        // マッチング後、自分自身のネットワークオブジェクトを生成する
+        var v = new Vector3(0, 0, 0);
+        PhotonNetwork.Instantiate("ObjectManagerPrefab", v, Quaternion.identity);
+    }
+}
